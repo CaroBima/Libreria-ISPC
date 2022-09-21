975b951620d5ba1df7fe2e6e5dbd3200928f18ea
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-09-21 20:46:07 -0300

    Comenzando con el modelado de las clases del back

--- library SpringBoot/src/main/java/com/ispc/library/model/User.java
+++ library SpringBoot/src/main/java/com/ispc/library/model/User.java
@@ -1,11 +1,29 @@
-
-
 package com.ispc.library.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  *
  * @author Caro
  */
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    private int id;
+    private String name;
+    private String lastNameP;
+    private String lastNameM;
+    private String domicilio;
+    private String tel;
+    private int sanctions;
+    private int sanc_money;
+    
+    
+    
     
 }
